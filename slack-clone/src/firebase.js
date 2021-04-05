94f33d6adb0657e41d1911c06904ff69a71f6e03
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-04-05 09:15:09 +0100

    setting up app

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAPVPdW0Gi_9V3nq2XqkptuQvddJnhyPE",
+    authDomain: "slack-b63ee.firebaseapp.com",
+    projectId: "slack-b63ee",
+    storageBucket: "slack-b63ee.appspot.com",
+    messagingSenderId: "1065402963485",
+    appId: "1:1065402963485:web:f351684a9234d8890b98f4",
+    measurementId: "G-VT5MW9DV5F"
+  };
